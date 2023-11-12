1874fc5bf9aed32b035836a5bf1015a032f2ec24
belka <vfrosenyuk@gmail.com>
2023-11-13 02:45:34 +0400

    added block selection

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,8 +2,22 @@ const swiper = new Swiper('.js-main-slider', {
     slidesPerView: 'auto',
     spaceBetween: 16,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+        nextEl: '.slider-navigation__button--main.swiper-button-next',
+        prevEl: '.slider-navigation__button--main.swiper-button-prev'
+    },
+    breakpoints: {
+        768: {
+            spaceBetween: 20,
+        },
+    }
+});
+
+const swiper2 = new Swiper('.js-selection-slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    navigation: {
+        nextEl: '.slider-navigation__button--selection.swiper-button-next',
+        prevEl: '.slider-navigation__button--selection.swiper-button-prev'
     },
     breakpoints: {
         768: {
